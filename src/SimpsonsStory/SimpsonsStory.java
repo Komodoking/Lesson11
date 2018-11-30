c03a73693248f0129c951448adb9f1a5a5a91806
ajdy5510 <ajdy5510@ISS2TWQKH2.scdsb.pri>
2018-11-30 14:25:46 -0500

    started Simpsons

--- src/SimpsonsStory/SimpsonsStory.java
+++ src/SimpsonsStory/SimpsonsStory.java
@@ -10,17 +10,32 @@ package SimpsonsStory;
  * @author ajdy5510
  */
 public class SimpsonsStory {
-static String start[]={"Simpsons in Cave","Lisa Trapped with Burns","Burns at Moe's","Moe with Edna","Edna in School"};
-static String end[]={"Simpsons leave cave","Lisa takes picture with Burns","Moe puts gold in machine","Edna stays on as teaacher"};
+
+    static String start[] = {"Simpsons in Cave", "Lisa Trapped with Burns", "Burns at Moe's", "Moe with Edna", "Edna in School"};
+    static String end[] = {"Simpsons leave cave", "Lisa takes picture with Burns", "Moe puts gold in machine", "Edna stays on as teacher"};
+
     public static void main(String[] args) {
-        if(story(0)){
-            for (int i = 0; i < 3; i++) {
-                System.out.println(start[i]);
-            }
+        for (int i = 0; i <5; i++) {
+            System.out.println("\n"+start[story(i)]);
+            int n=i;
+         
+            if(n==4){
+                while(true){
+                    
+                n--;
+                System.out.println("\n"+end[story(i)]);
+           if(n==0){
+               break;
+           }
+                }
         }
+
+    
+
+}
+
+}public static int story(int s) {
+
+        return s;
     }
-  public int story(int s){
-      return s;
-  }
-  
 }
