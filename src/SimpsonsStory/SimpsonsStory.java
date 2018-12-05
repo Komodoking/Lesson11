1255af4487c9ca185691102df7625405640740b4
ajdy5510 <ajdy5510@ISS2TWQKH2.scdsb.pri>
2018-12-05 14:17:24 -0500

    ?????

--- src/SimpsonsStory/SimpsonsStory.java
+++ src/SimpsonsStory/SimpsonsStory.java
@@ -12,30 +12,18 @@ package SimpsonsStory;
 public class SimpsonsStory {
 
     static String start[] = {"Simpsons in Cave", "Lisa Trapped with Burns", "Burns at Moe's", "Moe with Edna", "Edna in School"};
-    static String end[] = {"Simpsons leave cave", "Lisa takes picture with Burns", "Moe puts gold in machine", "Edna stays on as teacher"};
+    static String end[] = {"Simpsons leave cave", "Lisa takes picture with Burns","Burns gets gold ","Moe puts gold in machine", "Edna stays on as teacher"};
 
     public static void main(String[] args) {
-        for (int i = 0; i <5; i++) {
-            System.out.println("\n"+start[story(i)]);
-            int n=i;
-         
-            if(n==4){
-                while(true){
-                    
-                n--;
-                System.out.println("\n"+end[story(i)]);
-           if(n==0){
-               break;
-           }
-                }
-        }
-
+        story(0);
+        
     
 
+
+
+}public static void story(int s) {
+            System.out.println(start[s]);
+ if(s<3) story(s+1);
+            System.out.println(end[s]);
 }
-
-}public static int story(int s) {
-
-        return s;
-    }
 }
