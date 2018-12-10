2ce233eaa090e26b6623aa3277d65f260244ecbc
ajdy5510 <ajdy5510@ISS2TWQKH2.scdsb.pri>
2018-12-10 14:33:11 -0500

    Kermit

--- src/BunnyEars/BunnyEars.java
+++ src/BunnyEars/BunnyEars.java
@@ -5,10 +5,36 @@
  */
 package BunnyEars;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ajdy5510
  */
 public class BunnyEars {
-    
+    ArrayList<Integer>testdata;
+    ArrayList<Integer> expected;
+    String id;
+    BunnyEars(ArrayList<Integer>al,ArrayList<Integer>ex,String name){
+        testdata=al;
+        expected=ex;
+        id=name;
+    }
+    public int solve(int bunnies){
+        if(bunnies==0){
+            return 0;
+        }
+        else{
+            return 2+solve(bunnies-1);
+        }
+        
+    }
+    public void test(){
+        System.out.println("Expected\t\tRun\tResult");
+        System.out.println("======================");
+        for (int i = 0; i < testdata.get(i); i++) {
+            String output=id+"("+testdata.get(i)+")->"+expected.get(i);
+        }
+        
+    }
 }
