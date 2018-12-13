8765d63706d0403912712528efd3be7ff05a9bcf
ajdy5510 <ajdy5510@ISS2TWQKH2.scdsb.pri>
2018-12-13 14:34:12 -0500

    Smashtime

--- src/BunnyEars/BunnyEars.java
+++ src/BunnyEars/BunnyEars.java
@@ -6,11 +6,6 @@
 package BunnyEars;
 
 import java.util.ArrayList;
-
-/**
- *
- * @author ajdy5510
- */
 public class BunnyEars {
     ArrayList<Integer>testdata;
     ArrayList<Integer> expected;
@@ -20,6 +15,17 @@ public class BunnyEars {
         expected=ex;
         id=name;
     }
+    public static void main(String args[]){
+ArrayList<Integer> in=new ArrayList();
+ArrayList<Integer> out=new ArrayList();
+in.add(0); in.add(1);in.add(2);
+in.add(3);in.add(4);in.add(5);
+in.add(12);in.add(50);in.add(234);
+out.add(0);out.add(1);out.add(2);
+out.add(3);out.add(4);out.add(5);
+out.add(24);out.add(100);out.add(468);
+BunnyEars prob=new BunnyEars(in,out,"BunnyEars");
+prob.test();}
     public int solve(int bunnies){
         if(bunnies==0){
             return 0;
@@ -29,12 +35,43 @@ public class BunnyEars {
         }
         
     }
+ 
+public int array11(int[] nums, int index) {
+
+  if (index >= nums.length) return 0;
+
+  if (nums[index] == 11) return 1 + array11(nums,index+1);
+
+  else return array11(nums,index+1);
+
+}
+
+    public int countPairs(String str) {
+int result = 0;
+if(str.length() <= 2) {
+return 0;
+}
+
+if(str.charAt(0) == str.charAt(2)) {
+result++;
+}
+result += countPairs(str.substring(1, str.length()));
+return result;
+}
     public void test(){
         System.out.println("Expected\t\tRun\tResult");
         System.out.println("======================");
         for (int i = 0; i < testdata.get(i); i++) {
             String output=id+"("+testdata.get(i)+")->"+expected.get(i);
+            System.out.println(String.format("%-25a", output));
+            int result=solve(testdata.get(i));
+            String status=result==expected.get(i)?"Ok":"Fail";
+            System.out.println(result+"\t"+status);
+            
         }
         
     }
+    
+
+
 }
